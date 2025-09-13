91b2dc31cb5ddd51508d370c69fc3cff97291db5
Sabrina Yen-Ko <59457991+syenko@users.noreply.github.com>
2025-09-13 14:02:23 -0700

    don't remove carriage returns

--- logger/utilities.py
+++ logger/utilities.py
@@ -1,21 +1,12 @@
 import re
 
 ANSI_ESCAPE_RE = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-
-def clean_ansi(text):
-    text = ANSI_ESCAPE_RE.sub('', text)
-    return text.replace('\r', '')  # optional: remove carriage returns
-
-def remove_bracketed_paste(text):
-    return text.replace("\x1B[200~", "").replace("\x1B[201~", "")
-  
+ 
 def clean_terminal_input(raw_text):
     # Remove bracketed paste markers
     text = raw_text.replace("\x1B[200~", "").replace("\x1B[201~", "")
     # Remove other ANSI escape codes
     text = ANSI_ESCAPE_RE.sub('', text)
-    # Remove carriage returns
-    text = text.replace('\r', '')
     return text
 
 def is_physical_enter(byte):
